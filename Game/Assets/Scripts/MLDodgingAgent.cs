25e75cef461408a007471ae287b90d08df60421e
Hans Gunther <hnsgunther@gmail.com>
2022-03-19 12:14:04 -0600

    Stubbed out the functions we will need to overload

---
+++ Game/Assets/Scripts/MLDodgingAgent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class MLDodgingAgent : Agent
+{
+    void Start()
+    {
+        
+    }
+
+    public override void OnEpisodeBegin(){
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor){
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers){
+
+    }
+
+}
---
+++ Game/Assets/Scripts/MLThrowingAgent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class MLThrowingAgent : Agent
+{
+        void Start()
+    {
+        
+    }
+
+    public override void OnEpisodeBegin(){
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor){
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers){
+
+    }
+
+
+}
