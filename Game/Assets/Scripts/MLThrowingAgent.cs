9f904d241e687e15c5c58004d4e6976190915dfb
Hyrum Gunther <hygunth@gmail.com>
2022-03-19 13:39:10 -0600

    On Action Received added to thrower

--- Game/Assets/Scripts/MLThrowingAgent.cs
+++ Game/Assets/Scripts/MLThrowingAgent.cs
@@ -7,9 +7,10 @@ using Unity.MLAgents.Actuators;
 
 public class MLThrowingAgent : Agent
 {
-        void Start()
+    GameState gameState;
+    void Start()
     {
-        
+        gameState = (GameState)FindObjectOfType<GameState>();
     }
 
     public override void OnEpisodeBegin(){
@@ -21,8 +22,22 @@ public class MLThrowingAgent : Agent
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers){
+        float speed = Mathf.Lerp(1.5f, 10f, actionBuffers.ContinuousActions[0]);
+        float angle = Mathf.Lerp(Mathf.Deg2Rad*-105f, Mathf.Deg2Rad*-75f, actionBuffers.ContinuousActions[1]);
+        float scale = Mathf.Lerp(0.5f, 1.5f, actionBuffers.ContinuousActions[2]);
+        float startX = Mathf.Lerp(-5f, 5f, actionBuffers.ContinuousActions[3]);
+       
+        Vector2 vel = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * speed;
 
+        gameState.Launch(vel, startX, scale);
     }
 
+    public override void Heuristic(in ActionBuffers actionsOut){
+        var continuousActionsOut = actionsOut.ContinuousActions;
 
+        continuousActionsOut[0] = Random.Range(0f,1f); //Speed
+        continuousActionsOut[1] = Random.Range(0f,1f); //Angle
+        continuousActionsOut[2] = Random.Range(0f,1f); //Scale
+        continuousActionsOut[3] = Random.Range(0f,1f); //Startx
+    }
 }
